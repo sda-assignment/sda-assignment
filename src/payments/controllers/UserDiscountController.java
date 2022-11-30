769ce9febeecd58312dccc77d9bda72132bb21de
Omar Brikaa <brikaaomar@gmail.com>
2022-11-30 16:25:06 +0200

    Account for used discounts in DiscountController

--- src/payments/controllers/UserDiscountController.java
+++ src/payments/controllers/UserDiscountController.java
@@ -1,19 +1,31 @@
 package payments.controllers;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 import payments.entities.Discount;
+import payments.entities.UsedDiscount;
 import datastore.Relation;
 
 public class UserDiscountController {
-    private Relation<Discount> relation;
+    private Relation<Discount> discountRelation;
+    private Relation<UsedDiscount> usedDiscountRelation;
 
     public UserDiscountController(Relation<Discount> relation) {
-        this.relation = relation;
+        this.discountRelation = relation;
     }
 
-    public ArrayList<Discount> getDiscounts() {
-        ArrayList<Discount> array = relation.select(d -> true);
-        return array;
+    public ArrayList<Discount> getDiscounts(String userEmail) {
+        ArrayList<Discount> allDiscounts = discountRelation.select(d -> true);
+        Collection<UsedDiscount> usedDiscounts = usedDiscountRelation.select(d -> d.email == userEmail);
+        Set<Integer> usedDiscountsIds = new HashSet<Integer>();
+        for (UsedDiscount d : usedDiscounts) {
+            usedDiscountsIds.add(d.discountId);
+        }
+        allDiscounts.removeIf(d -> usedDiscountsIds.contains(d.id));
+        return allDiscounts;
     }
 
 }

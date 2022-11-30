578d91ba4919d4467e08a31ce6be462454c60f71
Omar Brikaa <brikaaomar@gmail.com>
2022-11-30 16:08:09 +0200

    Refactor AdminRefundController

---
+++ src/payments/controllers/AdminRefundController.java
@@ -0,0 +1,43 @@
+package payments.controllers;
+
+import java.util.ArrayList;
+
+import datastore.Relation;
+import datastore.exceptions.EntitySaveException;
+import payments.common.Response;
+import payments.entities.RefundRequest;
+import payments.entities.User;
+import payments.entities.enums.RefundRequestStatus;
+
+public class AdminRefundController {
+    private Relation<RefundRequest> refundRelation;
+    private Relation<User> userRelation;
+
+    public AdminRefundController(Relation<RefundRequest> refundRelation, Relation<User> userRelation) {
+        this.userRelation = userRelation;
+        this.refundRelation = refundRelation;
+    }
+
+    public ArrayList<RefundRequest> getRefundRequests() {
+        return refundRelation.select(r -> true);
+    }
+
+    public Response acceptRefund(int rid) throws EntitySaveException {
+        refundRelation.update(
+                r -> new RefundRequest(r.id, r.amount, r.serviceName, RefundRequestStatus.ACCEPTED, r.userEmail),
+                r -> r.id == rid);
+
+        ArrayList<RefundRequest> targetRefund = refundRelation.select(r -> r.id == rid);
+        userRelation.update(
+                u -> new User(u.email, u.username, u.password, u.isAdmin, u.wallet + targetRefund.get(0).amount),
+                u -> u.email == targetRefund.get(0).userEmail);
+        return new Response(true, "Refund accepted, returning funds to wallet");
+    }
+
+    public Response rejectRefund(int rid) throws EntitySaveException {
+        refundRelation.update(
+                r -> new RefundRequest(r.id, r.amount, r.serviceName, RefundRequestStatus.REJECTED, r.userEmail),
+                r -> r.id == rid);
+        return new Response(true, "Refund rejected");
+    }
+}

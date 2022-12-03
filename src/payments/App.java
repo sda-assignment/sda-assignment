f0ba8994163e33c3f033cbccbfcbdc6e5fc6b776
Omar Brikaa <brikaaomar@gmail.com>
2022-12-03 21:25:34 +0200

    UserController, clean up Main.java

--- src/payments/App.java
+++ src/payments/App.java
@@ -1,65 +1,11 @@
 package payments;
 
-import common.Util;
-import datastore.Relation;
 import datastore.exceptions.EntityException;
 import datastore.exceptions.EntityLoadException;
 import datastore.exceptions.EntitySaveException;
-import payments.entities.RefundRequest;
-import payments.entities.Transaction;
-import payments.entities.User;
-import payments.entities.builders.RefundRequestBuilder;
-import payments.entities.builders.TransactionBuilder;
-import payments.entities.builders.UserBuilder;
-import payments.boundaries.AdminRefundBoundary;
-import payments.boundaries.Router;
 
 public class App {
     public static void main(String[] args) throws EntityException, EntitySaveException, EntityLoadException {
-        System.out.println("isPositiveFloat");
-        System.out.println(Util.isPositiveFloat("123"));
-        System.out.println(Util.isPositiveFloat("-123"));
-        System.out.println(Util.isPositiveFloat("123.2"));
-        System.out.println(Util.isPositiveFloat("+"));
-        System.out.println("isPositiveInt");
-        System.out.println(Util.isPositiveInt("123"));
-        System.out.println(Util.isPositiveInt("-123"));
-        System.out.println(Util.isPositiveInt("123.2"));
-        System.out.println(Util.isPositiveInt("+"));
-
-        Relation<User> userRelation = new Relation<User>("users", new UserBuilder());
-        // userRelation.insert(new User("ali", "asd", "asd", false, 12.2));
-        // userRelation.insert(new User("alix", "asd", "asd", false, 22.2));
-        // userRelation.insert(new User("alid", "asd", "asd", false, 15.2));
-        // double amount = userRelation.selectMax(u -> u.wallet);
-        // System.out.println(amount);
-        System.out.println(userRelation.select(u -> true));
-        userRelation.update(u -> new User(u.email, "aloka", u.password, u.isAdmin, u.wallet),
-                u -> u.username.equals("ali"));
-
-        Relation<Transaction> transactionRelation = new Relation<Transaction>("transactions", new TransactionBuilder());
-        // transactionRelation
-        // .insert(new Transaction(0, "waelwanas@gmail.com", LocalDateTime.now(), 0,
-        // TransactionType.REFUND));
-        // transactionRelation.insert(
-        // new Transaction(1, "ahmedwaelwanas@gmail.com", LocalDateTime.now(), 110,
-        // TransactionType.REFUND));
-        // transactionRelation.insert(
-        // new Transaction(2, "ahmedwanas@gmail.com", LocalDateTime.now(), 2132130,
-        // TransactionType.REFUND));
-
-        Relation<RefundRequest> refundRelation = new Relation<RefundRequest>("refundRequests",
-                new RefundRequestBuilder());
-        // refundRelation.insert(new RefundRequest(0, 0, RefundRequestStatus.PENDING,
-        // "ahmedwanas@gmail.com"));
-        // refundRelation.insert(new RefundRequest(1, 1, RefundRequestStatus.PENDING,
-        // "ahmedwaelwanas@gmail.com"));
-        // refundRelation.insert(new RefundRequest(2, 2, RefundRequestStatus.PENDING,
-        // "waelwanas@gmail.com"));
-
-        AdminRefundBoundary adminView = new AdminRefundBoundary(refundRelation, userRelation, transactionRelation);
-        Router r = new Router(adminView);
-        r.execute();
-
+        System.out.println("Hello World!");
     }
 }
---
+++ src/payments/controllers/UserController.java
@@ -0,0 +1,27 @@
+package payments.controllers;
+
+import common.Util;
+import datastore.Relation;
+import datastore.exceptions.EntitySaveException;
+import payments.common.Response;
+import payments.entities.User;
+
+public class UserController {
+    private Relation<User> userRelation;
+    private LogInSession logInSession;
+
+    public UserController(Relation<User> userRelation, LogInSession logInSession) {
+        this.userRelation = userRelation;
+        this.logInSession = logInSession;
+    }
+
+    public Response rechargeWallet(double amount, String cardNumber) throws EntitySaveException {
+        if (!Util.isPositiveInt(cardNumber))
+            return new Response(false, "Invalid card number");
+        if (amount < 0)
+            return new Response(false, "Invalid amount");
+        userRelation.update(u -> new User(u.email, u.username, u.password, u.isAdmin, u.wallet + amount),
+                u -> u.email == logInSession.getLoggedInUser().email);
+        return new Response(true, "Wallet recharged successfully");
+    }
+}
